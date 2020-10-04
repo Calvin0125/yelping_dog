2b9cc9d5cbb93af1f04de31d26bbc43e825ae3ce
Calvin Conley <cconley0125@gmail.com>
2020-10-04 15:54:12 -0400

    added menu pages, created dropdown menu

--- controller.rb
+++ controller.rb
@@ -2,8 +2,36 @@ require "sinatra"
 require "sinatra/content_for"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
 configure(:development) do
   require "sinatra/reloader"
+end
+
+before do
+  @wine_hash = {
+    red: [
+      "Screaming Eagle Cabernet Sauvignon, 1992, Oakville, Napa Valley, CA",
+      "DRC Romanee-Conti, 1995, Vosne-Romaneè, Burgundy, France",
+      "Vietti Ravera, 2001, Barolo, Italy",
+      "Chateau Petrus, 1975, Pomerol, Bourdeaux, France",
+      "Penfolds Grange, 1999, South Australia"
+    ],
+    white: [
+      "Domaine Leflaive Montrachet, 2007, Puligny-Montrachet, Burgundy, France",
+      "Chateau Montalena Chardonnay 2012, Napa Valley, CA",
+      "Domaine Georges Vernay, 2017, Condrieu, Rhone, France",
+      "Egon Müller Scharzhofberger Riesling Auslese, 2014, Mosel, Germany",
+      "Seresin Marama Sauvignon Blanc, 2013, Marlborough, New Zealand"
+    ],
+    bubbly: [
+      "Dom Perignon Brut, 2002, Champagne, France",
+      "Schramsberg Brut Rose, 2017, Napa Valley, CA"
+    ]
+  }
 end
 
 get "/" do
@@ -16,4 +44,8 @@ end
 
 get "/about" do
   erb :about, layout: :layout
+end
+
+get "/wine_menu" do
+  erb :wine_menu, layout: :layout
 end
